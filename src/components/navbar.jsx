bf6681f42e53e7770fbc3383a3db918c746879e1
glaucecassiano <aylastankovsky@gmail.com>
2023-10-22 14:49:29 -0300

    liderada edits 1

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,39 +1,34 @@
-import {Link} from "react-router-dom"
-import './navbar.css'
+import { Link } from "react-router-dom";
+import "./navbar.css";
 import { useNavigate } from "react-router-dom";
 
-
 export const Navbar = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const CadastroPage = (event) => {
+    event.preventDefault();
 
-    const CadastroPage = (event) => {
-        event.preventDefault();
+    return navigate("/cadastro");
+  };
 
-        return navigate("/cadastro");
-    }
+  return (
+    <nav className="nav">
+      <h1>LíderAda</h1>
 
-
-
-    return(
-        <nav className="nav">
-
-            <h1>Lider Ada</h1>
-
-            <ul>
-                <li>
-                <Link to="/">Categorias</Link>
-                </li>
-                <li>
-                <Link to="/forum">Fórum</Link>
-                </li>
-                <li>
-                <Link to="/materiais">Materiais</Link>
-                </li>
-                <div>
-                    <button onClick={CadastroPage}>Faça Parte</button>
-                </div>
-            </ul>
-        </nav>
-    )
-}
+      <ul>
+        <li>
+          <Link to="/">Categorias</Link>
+        </li>
+        <li>
+          <Link to="/forum">Fórum</Link>
+        </li>
+        <li>
+          <Link to="/materiais">Materiais</Link>
+        </li>
+        <div>
+          <button onClick={CadastroPage}>Faça Parte</button>
+        </div>
+      </ul>
+    </nav>
+  );
+};
--- src/pages/forum.jsx
+++ src/pages/forum.jsx
@@ -8,62 +8,63 @@ export const Forum = () => {
   return (
     <div>
       <div>
-        <Navbar></Navbar>
-      </div>
-      <div id="blocoum">
-        <div id="entradaum">
-          <h2>Remuneração</h2>
-          <img id="marcador" src={Marcador} alt="marcador" />
-          <p>Papos sobre #salários, #cargos e #equidade</p>
-          <h2>Remuneração</h2>
-          <img id="marcador" src={MarcadorVazio} alt="marcador" />
-
-          <p>Papos sobre #salários, #cargos e #equidade</p>
-          <h2>Remuneração</h2>
-          <img id="marcador" src={MarcadorVazio} alt="marcador" />
-
-          <p>Papos sobre #salários, #cargos e #equidade</p>
-          <h2>Remuneração</h2>
-          <img id="marcador" src={MarcadorVazio} alt="marcador" />
-
-          <p>Papos sobre #salários, #cargos e #equidade</p>
-          <h2>Remuneração</h2>
-          <img id="marcador" src={MarcadorVazio} alt="marcador" />
-
-          <p>Papos sobre #salários, #cargos e #equidade</p>
+        <div>
+          <Navbar></Navbar>
         </div>
-        <div id="entradadois"></div>
-        <h1>Maternidade</h1>
-        <img id="marcador" src={MarcadorVazio} alt="marcador" />
+        <div id="bloco um">
+          <div id="entradaum">
+            <h2>Remuneração</h2>
+            <img id="marcador" src={Marcador} alt="marcador" />
+            <p>Papos sobre #salários, #cargos e #equidade</p>
+            <h2>Remuneração</h2>
+            <img id="marcador" src={MarcadorVazio} alt="marcador" />
 
-        <p>
-          Este tópico aborda questões relacionadas a maternidade e trabalho,
-          jornada dupla, cultura organizacional e afins.
-        </p>
-        <div></div>
+            <p>Papos sobre #salários, #cargos e #equidade</p>
+            <h2>Remuneração</h2>
+            <img id="marcador" src={MarcadorVazio} alt="marcador" />
 
-        <div id="blocodois">
-          <h1>#TópicosPopulares</h1>
-          <div id="banner">
-            <div id="textobanner">
-              <h3>Banner</h3>
-              <p>imagem + titulo do tópico</p>
+            <p>Papos sobre #salários, #cargos e #equidade</p>
+            <h2>Remuneração</h2>
+            <img id="marcador" src={MarcadorVazio} alt="marcador" />
+
+            <p>Papos sobre #salários, #cargos e #equidade</p>
+            <h2>Remuneração</h2>
+            <img id="marcador" src={MarcadorVazio} alt="marcador" />
+
+            <p>Papos sobre #salários, #cargos e #equidade</p>
+          </div>
+          <div id="entradadois"></div>
+          <h1>Maternidade</h1>
+          <img id="marcador" src={MarcadorVazio} alt="marcador" />
+
+          <p>
+            Este tópico aborda questões relacionadas a maternidade e trabalho,
+            jornada dupla, cultura organizacional e afins.
+          </p>
+          <div></div>
+
+          <div id="blocodois">
+            <h1>#TópicosPopulares</h1>
+            <div id="banner">
+              <div id="textobanner">
+                <h3>Banner</h3>
+                <p>imagem + titulo do tópico</p>
+              </div>
+            </div>
+
+            <div id="seis"></div>
+
+            <div id="banner">
+              <div id="textobanner">
+                <h3>Banner</h3>
+                <p>imagem + titulo do tópico</p>
+              </div>
             </div>
           </div>
-
-          <div id="seis"></div>
-
-          <div id="banner">
-            <div id="textobanner">
-              <h3>Banner</h3>
-              <p>imagem + titulo do tópico</p>
-            </div>
-          </div>
+          <div id="footerum"></div>
         </div>
-        <h1>Junte-se a nós</h1>
-        <div></div>
-        <Footer></Footer>
       </div>
+      <Footer></Footer>
     </div>
   );
 };
