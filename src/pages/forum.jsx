64f4a089994688eca5a960408ad84112cdc554c2
glaucecassiano <aylastankovsky@gmail.com>
2023-10-22 12:57:35 -0300

    forum inicio

--- src/pages/forum.jsx
+++ src/pages/forum.jsx
@@ -1,17 +1,69 @@
 import { Footer } from "../components/footer";
 import { Navbar } from "../components/navbar";
+import "./forum.css";
+import Marcador from "../assets/marcador.svg";
+import MarcadorVazio from "../assets/marcadorvazio.svg";
 
+export const Forum = () => {
+  return (
+    <div>
+      <div>
+        <Navbar></Navbar>
+      </div>
+      <div id="blocoum">
+        <div id="entradaum">
+          <h2>Remuneração</h2>
+          <img id="marcador" src={Marcador} alt="marcador" />
+          <p>Papos sobre #salários, #cargos e #equidade</p>
+          <h2>Remuneração</h2>
+          <img id="marcador" src={MarcadorVazio} alt="marcador" />
 
-export const Forum =() => {
+          <p>Papos sobre #salários, #cargos e #equidade</p>
+          <h2>Remuneração</h2>
+          <img id="marcador" src={MarcadorVazio} alt="marcador" />
 
-    return (
-        <div>
-            <div>
-            <Navbar></Navbar>
-            </div>
-            <div>
-                <Footer></Footer>
-            </div>
+          <p>Papos sobre #salários, #cargos e #equidade</p>
+          <h2>Remuneração</h2>
+          <img id="marcador" src={MarcadorVazio} alt="marcador" />
+
+          <p>Papos sobre #salários, #cargos e #equidade</p>
+          <h2>Remuneração</h2>
+          <img id="marcador" src={MarcadorVazio} alt="marcador" />
+
+          <p>Papos sobre #salários, #cargos e #equidade</p>
         </div>
-    )
-}
+        <div id="entradadois"></div>
+        <h1>Maternidade</h1>
+        <img id="marcador" src={MarcadorVazio} alt="marcador" />
+
+        <p>
+          Este tópico aborda questões relacionadas a maternidade e trabalho,
+          jornada dupla, cultura organizacional e afins.
+        </p>
+        <div></div>
+
+        <div id="blocodois">
+          <h1>#TópicosPopulares</h1>
+          <div id="banner">
+            <div id="textobanner">
+              <h3>Banner</h3>
+              <p>imagem + titulo do tópico</p>
+            </div>
+          </div>
+
+          <div id="seis"></div>
+
+          <div id="banner">
+            <div id="textobanner">
+              <h3>Banner</h3>
+              <p>imagem + titulo do tópico</p>
+            </div>
+          </div>
+        </div>
+        <h1>Junte-se a nós</h1>
+        <div></div>
+        <Footer></Footer>
+      </div>
+    </div>
+  );
+};
