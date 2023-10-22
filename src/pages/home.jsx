7337bbd7caddcf7ae8608335165068e6d50cbea7
glaucecassiano <aylastankovsky@gmail.com>
2023-10-22 12:32:59 -0300

    css e home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -11,37 +11,82 @@ export const Home = () => {
       <div className="vertical">
         <div id="um">
             <div >
-            <h2>Somos a LíderAda</h2>
-            <p>Uma comunidade feita por e para <br></br> mulheres, líderes empreendedoras e dispostas a trocar conhecimentos.</p>
+            <h2 id="titulo">Somos a LíderAda</h2>
             </div>
         </div>
 
         <div id="dois">
-          <p>objetivo da comunindade, link quem somos</p>
-        </div>
+<p>Nossa comunidade  <strong>acolhe</strong> mulheres, <strong>incentiva</strong> o desenvolvimento profissional e <strong>fortalece</strong> as conexões que são o futuro.</p>        
+</div>
 
         <div id="tres">
-          <p> Dados sobre liderança feminina</p>
+            <h2>
+            Onde estão as lideranças?
+            </h2>
+          <p>O Brasil tem apenas 38% dos cargos de liderança ocupados por mulheres – e esse número é ainda menor quando olhamos para o âmbito político. Queremos mudar esse cenário e criar um espaço seguro para que mais de nós possam mudar esses números até 2030.</p>
+          <div>
+
+<div className="quatro">
+
+            <div className="dadosum">
+                <h3 className="porcentagem">38%</h3>
+            </div>
+                <div className="dadosdois">
+                    <p>Mulheres em cargos de liderança*</p>
+                </div>
+
+
+            <div className="dadosum">
+            <h3 className="porcentagem">38%</h3>
+
+            </div>
+            <div className="dadosdois">
+            <p>Mulheres em cargos de liderança*</p>
+
+
+
+
+            </div>
+            <div className="dadosum">
+            <h3 className="porcentagem">38%</h3>
+
+            </div>
+            <div className="dadosdois"></div>
+            <p>Mulheres em cargos de liderança*</p>
+
+
+          </div>
         </div>
-
-        <div className="horizontal">
-          <div id="quatro"></div>
-
-          <p>tópicos quentes</p>
-
+        <p>*Fonte:</p>
+        </div>
+        <div className="vertical">
           <div id="cinco"></div>
 
-          <p>tópicos quentes</p>
+          <h1>#TópicosPopulares</h1>
+
+          <div id="banner">
+            <div id="textobanner">
+                <h3>Banner</h3>
+                <p>imagem + titulo do tópico</p>
+            </div>
+          </div>
+
+          <div id="seis"></div>
+
+          <div id="banner">
+          <div id="textobanner">
+                <h3>Banner</h3>
+                <p>imagem + titulo do tópico</p>
+            </div>
+          </div>
+
+
         </div>
 
-        <div id="seis">navegue pelas categorias, listas</div>
+        <div id="sete">navegue pelas categorias, listas</div>
 
-        <div id="sete">
+        <div id="oito">
           <p> assine nossa newsletter</p>
-        </div>
-
-        <div id="rodape">
-          <p> rodape </p>
         </div>
       </div>
       <div>
